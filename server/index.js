3ea4de2dc4363dc614ffeb3080e3166cf467e290
Hernan Parino <you@example.com>
2023-09-10 19:30:42 -0300

    cambios en el index alter true y quito el port5000

--- server/index.js
+++ server/index.js
@@ -1,18 +1,9 @@
-// const axios = require("axios");
-// const server = require("./src/server");
-// const { conn } = require('./src/db.js');
-// const PORT = 3001;
 
-// conn.sync({ force: true }).then(() => {
-// server.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// })
-// }).catch(error => console.error(error))
 
 const axios = require("axios"); // Importa el módulo axios para realizar solicitudes HTTP
 const server = require("./src/server"); // Importa tu servidor Express desde server.js
 const { conn } = require('./src/db.js'); // Importa la instancia de la base de datos de db.js
-const PORT = process.env.PORTSERVER; // Define el puerto en el que deseas que el servidor escuche
+const PORT = process.env.PORT; // Define el puerto en el que deseas que el servidor escuche
 const {getCountriesForAPI} = require('./src/controllers/getCountries')
 require('dotenv').config;
 
